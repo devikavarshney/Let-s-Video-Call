b5432ebdd064ba6f0deb9380346d0e06750394b7
devika <devikavarshney0003@gmail.com>
2023-04-07 14:47:15 +0530

    add functionality to answer call and allow others to stream their video

--- server.js
+++ server.js
@@ -1,31 +1,30 @@
-const express = require('express');
-const app= express();
-const server = require('http').Server(app);
-const {v4:uuid}=require('uuid');
-const io = require('socket.io')(server);
-const {ExpressPeerServer} = require('peer');
-const peerServer = ExpressPeerServer(server,{
-    debug:true
-})
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const { v4: uuid } = require("uuid");
+const io = require("socket.io")(server);
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
 
-app.set('view engine','ejs');
-app.use(express.static('public'));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use("/peerjs", peerServer);
 
+app.get("/", (req, res) => {
+  res.redirect(`/${uuid()}`);
+});
 
-app.get('/',(reg,res)=>{
-    res.redirect(`/${uuid()}`);
-})
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
 
-app.get('/:room', (req,res)=>{
-    res.render('room',{roomId:req.params.room});
-})
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).emit("user-connected", userId);
+  });
+});
 
-io.on('connection',socket =>{
-    socket.on('join-room', (roomId)=>{
-        socket.join(roomId);
-        socket.to(roomId).emit('user-connected');
-    })
-})
-
-
-server.listen(3030); 
+server.listen(3030);
